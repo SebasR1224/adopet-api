dcc3e9703ff3f56f52cd820ab1ff566a579440d8
SebasR1224 <jr7815611@gmail.com>
2024-04-30 21:07:35 -0500

    add baseline for animal report abandoned animal - create table and models

---
+++ database/seeders/AnimalReportAbandonedAnimalSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AnimalReportAbandonedAnimalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
