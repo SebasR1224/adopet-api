ba0aa6e574f09a6e6acc56c95b40b02fe601b741
SebasR1224 <jr7815611@gmail.com>
2024-05-05 19:19:52 -0500

    complete factory and seeder to animal entity

--- database/factories/AnimalFactory.php
+++ database/factories/AnimalFactory.php
@@ -2,6 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Animal;
+use App\Models\Especie;
+use App\Models\Foundation;
+use App\Models\Status;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +20,22 @@ class AnimalFactory extends Factory
      */
     public function definition(): array
     {
+        $especies = Especie::all();
+        $especie = $especies->random();
+
+        $status = Status::where('type', Animal::class)->get('code');
+
         return [
-            //
+            'name' => fake()->name(),
+            'image' => fake()->imageUrl(),
+            'description' => fake()->text(),
+            'age' => fake()->numberBetween(1,20),
+            'coat_color' => fake()->colorName(),
+            'especie' => $especie->name,
+            'race' => $especie->races()->get()->random()->name,
+            'weight' => fake()->randomFloat(),
+            'status' => $status->random()->code,
+            'gender' => fake()->randomElement(['MALE', 'FEMALE'])
         ];
     }
 }
