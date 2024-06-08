05e748db0750ee66a9770a59bb94fa23b5eff737
SebasR1224 <jr7815611@gmail.com>
2024-06-07 20:23:03 -0500

    added new endpoint

--- app/Http/Controllers/AnimalReports/AnimalReportManagementController.php
+++ app/Http/Controllers/AnimalReports/AnimalReportManagementController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\AnimalReports;
 
+use App\Constants\Messages\Success;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ReportAbandonedAnimalResource;
+use App\Http\Response\Response;
+use App\Models\ReportAbandonedAnimal;
 use Illuminate\Http\Request;
 
 class AnimalReportManagementController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return Response::success(Success::OPERATION_SUCCESSFUL, ReportAbandonedAnimalResource::collection(ReportAbandonedAnimal::all()));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AnimalReports\AnimalReportController;
+use App\Http\Controllers\AnimalReports\AnimalReportManagementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('/report-abandoned-animal', [AnimalReportController::class, 'reportAbandonedAnimal']);
+Route::get('/report-abandoned-animal', [AnimalReportManagementController::class, 'index']);
